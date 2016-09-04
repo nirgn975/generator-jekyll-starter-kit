1329022ba222d0dae394eaa95ec5e32b9b1f2aa9
Nir Galon <nirgn975@gmail.com>
2016-09-04 15:23:35 +0300

    Add list and checkbox options.

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,15 +7,65 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the kickass ' + chalk.red('generator-jekyll-starter-kit') + ' generator!'
+      'Welcome to the kickass ' + chalk.red('jekyll-starter-kit') + ' generator!'
     ));
 
     var prompts = [{
       type: 'confirm',
       name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      message: 'Would you like to enable this option?2',
       default: true
     }];
+
+    // var prompts = [{
+    //   type: 'checkbox',
+    //   name: 'features',
+    //   message: 'What do you want to make today?',
+    //   choices: [
+    //     {
+    //     name: ' a client.com project',
+    //     value: 'includeClient',
+    //     checked: true
+    //     }, {
+    //     name: ' a front-end project',
+    //     value: 'includeFrontEnd',
+    //     checked: false
+    //     }, {
+    //     name: ' a marketing project',
+    //     value: 'includeMarketing',
+    //     checked: false
+    //     }, {
+    //     name: ' an email project',
+    //     value: 'includeEmail',
+    //     checked: false
+    //     }
+    //   ]
+    // }];
+
+    // var prompts = [{
+    //   type: 'list',
+    //   name: 'features',
+    //   message: 'What do you want to make today?',
+    //   choices: [
+    //     {
+    //     name: ' a client.com project',
+    //     value: 'includeClient',
+    //     checked: true
+    //     }, {
+    //     name: ' a front-end project',
+    //     value: 'includeFrontEnd',
+    //     checked: false
+    //     }, {
+    //     name: ' a marketing project',
+    //     value: 'includeMarketing',
+    //     checked: false
+    //     }, {
+    //     name: ' an email project',
+    //     value: 'includeEmail',
+    //     checked: false
+    //     }
+    //   ]
+    // }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
@@ -25,8 +75,8 @@ module.exports = yeoman.Base.extend({
 
   writing: function () {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('my-awesome-site'),
+      this.destinationPath('my-awesome-site')
     );
   },
 
