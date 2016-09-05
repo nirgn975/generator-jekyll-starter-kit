93f405fe7fbdf75f81a03236ba511d5962188cb0
Nir Galon <nirgn975@gmail.com>
2016-09-05 08:47:20 +0300

    Add Jekyll site. (#2)
    * Add jekyll site.
    
    * Add environment files to gitignore, and delete the dummyfile.
    
    * Add list and checkbox options.
    
    * Add more prompts.
    
    * Remove jekyll plugins.

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,26 +7,72 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the kickass ' + chalk.red('generator-jekyll-starter-kit') + ' generator!'
+      'Welcome to the kickass ' + chalk.red('jekyll-starter-kit') + ' generator!'
     ));
 
     var prompts = [{
+      // Prompts the user to pick a templating engine.
+      type: 'list',
+      name: 'html',
+      message: 'What Template Engine do you want to use?',
+      choices: [
+        {
+        name: ' HTML',
+        value: 'html',
+        checked: true
+        }, {
+        name: ' Jade',
+        value: 'jade',
+        checked: false
+        }
+      ]
+    }, {
+      // Prompts the user to choose stylesheets.
+      type: 'list',
+      name: 'css',
+      message: 'What Stylesheets do you want to use?',
+      choices: [
+        {
+        name: ' CSS',
+        value: 'css',
+        checked: true
+        }, {
+        name: ' SASS',
+        value: 'sass',
+        checked: false
+        }, {
+        name: ' SCSS',
+        value: 'scss',
+        checked: false
+        }
+      ]
+    }, {
+      // Prompts the user to decide if he want offline support.
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      name: 'sw',
+      message: 'Would you like to enable Service Worker for offline use?',
+      default: true
+    }, {
+      // Prompts the user to decide if he want to validate his build.
+      type: 'confirm',
+      name: 'travis',
+      message: 'Would you like to enable HTMLProofer to validate your Jekyll output?',
       default: true
     }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      console.log(props);
+      this.props.someAnswer = 'foo';
     }.bind(this));
   },
 
   writing: function () {
+    console.log(this.props.someAnswer);
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('my-awesome-site'),
+      this.destinationPath('my-awesome-site')
     );
   },
 
