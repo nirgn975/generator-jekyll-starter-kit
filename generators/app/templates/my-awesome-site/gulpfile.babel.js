1b85dc28990bc65497970cfdcbcccf010fe157ab
Nir Galon <nirgn975@gmail.com>
2016-09-08 14:33:40 +0300

    Build Jekyll and minify HTML.

--- generators/app/templates/my-awesome-site/gulpfile.babel.js
+++ generators/app/templates/my-awesome-site/gulpfile.babel.js
@@ -1,6 +1,28 @@
 'use strict';
 
 import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+// Minify the HTML.
+gulp.task('minify-html', () => {
+  return gulp.src('_site/**/*.html')
+    .pipe($.htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeOptionalTags: true
+    }))
+    .pipe(gulp.dest('_site'))
+});
+
+gulp.task('jekyll-build', $.shell.task([ 'jekyll build' ]));
 
 // Default task.
 gulp.task('default', () => console.log('in jekyll build task'));
