042446caff57d8f912a18e3314a3376ca7e127c9
Nir Galon <nirgn975@gmail.com>
2016-09-11 15:06:03 +0300

    Mock new generator.

--- test/app.js
+++ test/app.js
@@ -7,12 +7,12 @@ describe('generator-jekyll-starter-kit:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({
-        project_name: 'projectName',
-        github_url: 'githubURL',
-        github_username: 'githubUserName',
-        project_description: 'projectDescription',
-        html: html.html,
-        css: css.css,
+        project_name: 'name',
+        github_url: 'url',
+        github_username: 'username',
+        project_description: 'description',
+        html: 'html',
+        css: 'css',
         es: true,
         sw: true,
         travis: true
