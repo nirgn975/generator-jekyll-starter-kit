f25e2a341edf4a296d71543462b769dfde2a2458
Nir Galon <nirgn975@gmail.com>
2016-09-11 10:44:58 +0300

    Try fix travis options.

--- test/app.js
+++ test/app.js
@@ -6,13 +6,15 @@ var helpers = require('yeoman-test');
 describe('generator-jekyll-starter-kit:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({
+        html: 'html',
+        css: 'css'
+      })
       .withPrompts({
         project_name: 'projectName',
         github_url: 'githubURL',
         github_username: 'githubUserName',
         project_description: 'projectDescription',
-        html: 'html',
-        css: 'css',
         es: true,
         sw: true,
         travis: true
