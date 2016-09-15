596110d9d7c75f1b99f5ba8db9b7623a281bd0f7
Nir Galon <nirgn975@gmail.com>
2016-09-15 08:46:37 +0300

    Add tests. (#16)
    * Add temp tests, for 100% coverage.
    
    * Refinement tests.

--- test/app.js
+++ test/app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-jekyll-starter-kit:app', function () {
-  this.timeout(10000);
+describe('generator-html-css-es2015-sw-travis', function () {
+  this.timeout(5000);
 
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
@@ -14,7 +14,7 @@ describe('generator-jekyll-starter-kit:app', function () {
         githubUsername: 'username',
         projectDescription: 'description',
         html: 'html',
-        css: 'css',
+        css: 'stylesheets',
         es: true,
         sw: true,
         travis: true
@@ -24,13 +24,33 @@ describe('generator-jekyll-starter-kit:app', function () {
 
   it('creates files', function () {
     assert.file([
+      '_includes',
+      '_layouts',
+      '_posts',
+      'css',
+      'images',
+      '_config.yml',
+      '.babelrc',
+      '.gitignore',
+      'about.md',
+      'feed.xml',
+      'Gemfile',
+      'gulpfile.babel.js',
+      'humans.txt',
       'index.html',
-      'README.md'
+      'LICENSE',
+      'manifest.json',
+      'manifest.webapp',
+      'package.json',
+      'README.md',
+      'robots.txt',
+      '.travis.yml'
     ]);
   });
 
   it('not created files', function () {
     assert.noFile([
+      '_includes-pug',
       'scss',
       'sass'
     ]);
@@ -38,11 +58,90 @@ describe('generator-jekyll-starter-kit:app', function () {
 
   it('fills package.json with correct information', function () {
     assert.JSONFileContent('package.json', {
-      name: 'name'
+      name: 'name',
+      description: 'description',
+      author: 'username',
+      repository: {
+        url: 'url'
+      }
     });
   });
 
   it('fills the README with project data', function () {
     assert.fileContent('README.md', 'name');
+  });
+
+  it('fills the LICENSE with project data', function () {
+    assert.fileContent('LICENSE', 'name');
+  });
+});
+
+describe('generator-pug-scss-travis', function () {
+  this.timeout(5000);
+
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        projectName: 'name',
+        githubUrl: 'url',
+        githubUsername: 'username',
+        projectDescription: 'description',
+        html: 'pug',
+        css: 'scss',
+        es: true,
+        sw: true,
+        travis: true
+      })
+      .toPromise();
+  });
+
+  it('creates files', function () {
+    assert.file([
+      '_includes-pug',
+      'scss',
+      '.travis.yml'
+    ]);
+  });
+
+  it('not created files', function () {
+    assert.noFile([
+      'css',
+      'sass'
+    ]);
+  });
+});
+
+describe('generator-pug-sass-no_travis', function () {
+  this.timeout(5000);
+
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        projectName: 'name',
+        githubUrl: 'url',
+        githubUsername: 'username',
+        projectDescription: 'description',
+        html: 'pug',
+        css: 'sass',
+        es: true,
+        sw: true,
+        travis: false
+      })
+      .toPromise();
+  });
+
+  it('creates files', function () {
+    assert.file([
+      '_includes-pug',
+      'sass'
+    ]);
+  });
+
+  it('not created files', function () {
+    assert.noFile([
+      'css',
+      'scss',
+      '.travis.yml'
+    ]);
   });
 });
