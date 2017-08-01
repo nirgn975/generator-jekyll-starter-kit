659dc0dc8c36de9e573e01bd6d53c55ec5999e42
Nir Galon <nir@galon.io>
2017-08-01 13:30:38 +0300

    Clear the output website directory when build (#73)
    * Cleanup the _site directory when build
    
    * Add a feature to the features list
    
    * Update dependencies
    
    * Use npmInstall to not install with bower
    
    * Fix lighthouse
    
    * Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 'use strict';
-var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
-var coveralls = require('gulp-coveralls');
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
@@ -15,10 +12,6 @@ gulp.task('static', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
-
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
@@ -49,14 +42,4 @@ gulp.task('watch', function () {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
-
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
-});
-
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test', 'coveralls']);
+gulp.task('default', ['static', 'test']);
